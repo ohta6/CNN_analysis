1d73fbe8a3d1df1d8cea43ba93af6b89a4bc153c
tama626 <no34499@gmail.com>
2018-05-23 13:05:28 +0900

    t-SNE

--- vgg.py
+++ vgg.py
@@ -1,9 +1,17 @@
 # encode:utf-8
 
 from chainer.links import VGG16Layers
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
 
 model = VGG16Layers()
 for links in model.links():
-    print(links.conv1_1.W.data.shape)
+    link1 = links.conv1_1.W.data.shape
     print(links.conv1_2.W.data.shape)
     break
+
+# link1.shape = (n, 9)となるようにreshape
+link1_reshape = link1.reshape((-1,9))
+link1_reduced = TSNE(n_components=2, random_state=0).fit_transform(link1_reshape)
+
+plt.scatter(link1_reduced[:,0], link1_reduced[:,1])
